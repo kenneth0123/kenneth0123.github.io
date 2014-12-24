4fcc5db699055eb156344f7c3f1f61c2a4ceff9c
kenneth0123 <kenbalv@gmail.com>
2014-12-23 19:05:08 -0800

    latest 12/23

--- js/hubindex.js
+++ js/hubindex.js
@@ -2,6 +2,14 @@ $(document).ready(function(){
 
 	//When use clicks Page 1
 	$("#page1").click(function(){
-		alert("You clicked page one");
+		window.location.href = "www.google.com";
+	});
+	
+	$("#page2").click(function(){
+		window.location.href = "www.facebook.com";
+	});
+	
+	$("#page3).click(function() {
+		window.location.href = "www.youtube.com";
 	});
 });
