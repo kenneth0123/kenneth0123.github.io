b627c7a78f14709180925221b7e416c3ef083cd4
kenneth0123 <kenbalv@gmail.com>
2015-01-21 15:56:45 -0800

    1/21

--- js/hubindex.js
+++ js/hubindex.js
@@ -17,10 +17,28 @@ $(document).ready(function(){
 	
 	$("#page4").click(function() {
 		window.location.href = "mypage.html";
+		
+		//show Interests upon loading page
+		$("myResume").hide(1000);
+		$("myAbout").hide(1000);
 	});
 	
 	//Close everything else and open only the clicked link
 	$("#resumeLink").click(function() {
-		//$("myResume").show();
+		$("myResume").show(1000);
+		$("myInterest").hide(1000);
+		$("myAbout").hide(1000);
+	});
+	
+	$("#aboutLink").click(function() {
+		$("myResume").hide(1000);
+		$("myInterest").hide(1000);
+		$("myAbout").show(1000);
+	});
+	
+	$("#interestLink").click(function() {
+		$("myResume").hide(1000);
+		$("myInterest").show(1000);
+		$("myAbout").hide(1000);
 	});
 });
