929c0ed35f647f637ade2a6c8d9ab17a62c0aa12
kenneth0123 <kenbalv@gmail.com>
2015-01-21 17:09:37 -0800

    1/21

--- js/mypage.js
+++ js/mypage.js
@@ -1,26 +1,9 @@
 $(document).ready(function(){
-
-	//When use clicks Google
-	$("#page1").click(function(){
-		window.location.href = "http://www.google.com";
-	});
-	
-	//When use clicks Facebook
-	$("#page2").click(function(){
-		window.location.href = "http://www.facebook.com";
-	});
-	
-	//When use clicks Youtube
-	$("#page3").click(function() {
-		window.location.href = "http://www.youtube.com";
-	});
-	
-	$("#page4").click(function() {
-		window.location.href = "mypage.html";
-		
-	});
-	
 	//MY PAGE JS
+	
+	$(".myResume").hide();
+	$(".myAbout").hide();
+
 	//Close everything else and open only the clicked link
 	$("#resumeLink").click(function() {
 		$(".myResume").show(1000);
